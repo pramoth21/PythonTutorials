a39140b32c337e4ec51d8e95020932aa9236b717
Pramoth Piumal <pramothpiumal7@gmail.com>
2026-05-23 15:18:00 +0530

    string methods in Python

---
+++ 05_string_methods/stringMethods.py
@@ -0,0 +1,8 @@
+name = " Pramoth piumal"
+
+print(name.lower())
+print(name.upper())
+print(name.title())
+print(name.strip()) #lstrip(), rstrip()
+print(name.find("piu"))
+print(name.replace("Pramoth", "Eren"))
